813231247a8ff974fd7848271247112fa12318ee
Borovsky0 <borovsky2000@gmail.com>
2023-10-03 02:40:48 +0300

    global changes, close #2, close #3

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,11 +1,43 @@
 import React, { useEffect, useState } from "react";
-import Axios from "axios";
-import { useTable, useSortBy } from "react-table";
+import Axios from "axios"
 import "./styles/global.css";
 
 function Home() {
   const [search, setSearch] = useState("");
   const [crypto, setCrypto] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: "", direction: "" });
+
+  const sortTable = (key, direction) => {
+    const sortedCrypto = [...crypto];
+
+    sortedCrypto.sort((a, b) => {
+      if (a[key] < b[key]) {
+        return direction === "asc" ? -1 : 1;
+      }
+      if (a[key] > b[key]) {
+        return direction === "asc" ? 1 : -1;
+      }
+      return 0;
+    });
+
+    setCrypto(sortedCrypto);
+  };
+
+  const handleSort = (key) => {
+    const direction = sortConfig.key === key && sortConfig.direction === "asc" ? "desc" : "asc";
+    setSortConfig({ key, direction });
+    sortTable(key, direction);
+  };
+
+  function convertCurrency(value) {
+    return Math.abs(Number(value)) >= 1.0e+9
+      ? (Math.abs(Number(value)) / 1.0e+9).toFixed(2) + "B"
+      : Math.abs(Number(value)) >= 1.0e+6
+        ? (Math.abs(Number(value)) / 1.0e+6).toFixed(2) + "M"
+        : Math.abs(Number(value)) >= 1.0e+3
+          ? (Math.abs(Number(value)) / 1.0e+3).toFixed(2) + "K"
+          : Math.abs(Number(value));
+  }
 
   useEffect(() => {
     Axios.get(
@@ -15,129 +47,67 @@ function Home() {
     });
   }, []);
 
-  function convertToInternationalCurrencySystem (labelValue) {
-
-    // Nine Zeroes for Billions
-    return Math.abs(Number(labelValue)) >= 1.0e+9
-
-    ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
-    // Six Zeroes for Millions 
-    : Math.abs(Number(labelValue)) >= 1.0e+6
-
-    ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + "M"
-    // Three Zeroes for Thousands
-    : Math.abs(Number(labelValue)) >= 1.0e+3
-
-    ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
-
-    : Math.abs(Number(labelValue));
-
-}
-
-  // Define the columns for your table
-  const columns = React.useMemo(
-    () => [
-      {
-        header: "Market Cap",
-        accessor: "marketCap",
-        cell: ({ row }) => (
-          <div className="info">
-            <div className="rank">
-            <p>{row.original.rank}</p>
-            </div>
-            <div className="logo">
-              <img src={row.original.icon} alt="logo" width="32px" />
-            </div>
-            <div className="symbol">
-              <p>{row.original.symbol}</p>
-            </div>
-            <div className="cap">
-              <p>{convertToInternationalCurrencySystem(row.original.marketCap)}</p>
-            </div>
-          </div>
-        ),
-      },
-      {
-        header: "Price",
-        accessor: "price",
-        cell: ({ value }) => `${(value.toFixed(2))}$`,
-      },
-      {
-        header: "24H %",
-        accessor: "priceChange1d",
-        cell: ({ value }) => (
-          <span className={value < 0 ? "red" : "green"}>{`${value}%`}</span>
-        ),
-      },
-    ],
-    []
-  );
-
-  const data = React.useMemo(() => {
-    return crypto.filter((val) =>
-      val.name.toLowerCase().includes(search.toLowerCase())
-    );
-  }, [crypto, search]);
-
-  // Initialize the table instance
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable(
-    {
-      columns,
-      data,
-    },
-    useSortBy // Enable sorting
-  );
-
   return (
     <div className="grid">
       <div className="statusBar"></div>
       <div className="top">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={search}
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
-      />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
       </div>
       <div className="body">
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render("header")}
-                  <span>
-                    {column.isSorted ? (column.isSortedDesc ? " ↓" : " ↑") : ""}
-                  </span>
-                </th>
-              ))}
+        <table>
+          <thead>
+            <tr>
+              <td onClick={() => handleSort("rank")}>Rank</td>
+              <td onClick={() => handleSort("marketCap")}>Market Cap</td>
+              <td onClick={() => handleSort("price")}>Price</td>
+              <td onClick={() => handleSort("priceChange1d")}>1d%</td>
             </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {crypto
+              .filter((val) => {
+                return val.name.toLowerCase().includes(search.toLowerCase());
+              })
+              .map((val, id) => {
+                return (
+                  <>
+                    <tr id={id}>
+                      <td className="td-default">
+                        <span className="rank">{val.rank}</span>
+                      </td>
+                      <td className="td-market-cap">
+                        <div className="market-cap-container">
+                          <div className="logo">
+                            <img src={val.icon} alt="logo" width={"24px"} />
+                          </div>
+                          <div className="symbol-and-cap-container">
+                            <p className="symbol">{val.symbol}</p>
+                            <span className="cap">{convertCurrency(val.marketCap)}</span>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="td-default">
+                        <p className="price">{"$" + (
+                          val.price > 1 ? val.price.toFixed(2) :
+                            val.price > 0.1 ? val.price.toFixed(4) :
+                              val.price > 0.01 ? val.price.toFixed(5) : val.price.toFixed(8))}</p>
+                      </td>
+                      <td className="td-default">
+                        <p className={val.priceChange1d < 0 ? "red" : "green"}>{val.priceChange1d}%</p>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+          </tbody>
+        </table>
       </div>
       <div className="bottom"></div>
       <div className="navigationBar"></div>
