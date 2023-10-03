5b45c437c046f12b5098da771bacdb857430ee0f
Borovsky0 <borovsky2000@gmail.com>
2023-10-03 22:16:16 +0300

    style changes, light/dark theme

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios"
 import "./styles/global.css";
+import bridge from '@vkontakte/vk-bridge';
 
 function Home() {
   const [search, setSearch] = useState("");
   const [crypto, setCrypto] = useState([]);
   const [sortConfig, setSortConfig] = useState({ key: "", direction: "" });
+  const [theme, setTheme] = useState("dark-theme");
+
+  const style = getComputedStyle(document.body);
+
+  const changeTheme = () => {
+    var color = "";
+    if (theme === "light-theme") {
+      setTheme("dark-theme")
+      color = "--black";
+    }
+    else { 
+      setTheme("light-theme")
+      color = "--white";
+    }
+    bridge.send('VKWebAppSetViewSettings', {
+      status_bar_style: 'dark',
+      action_bar_color: style.getPropertyValue(color),
+      navigation_bar_color: style.getPropertyValue(color)
+    });
+  }
 
   const sortTable = (key, direction) => {
     const sortedCrypto = [...crypto];
@@ -48,69 +69,71 @@ function Home() {
   }, []);
 
   return (
-    <div className="grid">
-      <div className="statusBar"></div>
-      <div className="top">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-      </div>
-      <div className="body">
-        <table>
-          <thead>
-            <tr>
-              <td onClick={() => handleSort("rank")}>Rank</td>
-              <td onClick={() => handleSort("marketCap")}>Market Cap</td>
-              <td onClick={() => handleSort("price")}>Price</td>
-              <td onClick={() => handleSort("priceChange1d")}>1d%</td>
-            </tr>
-          </thead>
-          <tbody>
-            {crypto
-              .filter((val) => {
-                return val.name.toLowerCase().includes(search.toLowerCase());
-              })
-              .map((val, id) => {
-                return (
-                  <>
-                    <tr id={id}>
-                      <td className="td-default">
-                        <span className="rank">{val.rank}</span>
-                      </td>
-                      <td className="td-market-cap">
-                        <div className="market-cap-container">
-                          <div className="logo">
-                            <img src={val.icon} alt="logo" width={"24px"} />
+    <div className={theme}>
+      <div className="grid">
+        <div className="statusBar"></div>
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </div>
+        <div className="body">
+          <table>
+            <thead>
+              <tr>
+                <td onClick={() => handleSort("rank")}>Rank</td>
+                <td onClick={() => handleSort("marketCap")}>Market Cap</td>
+                <td onClick={() => handleSort("price")}>Price</td>
+                <td onClick={() => handleSort("priceChange1d")}>1d%</td>
+              </tr>
+            </thead>
+            <tbody>
+              {crypto
+                .filter((val) => {
+                  return val.name.toLowerCase().includes(search.toLowerCase());
+                })
+                .map((val, id) => {
+                  return (
+                    <>
+                      <tr className="tr-border" id={id}>
+                        <td className="td-default">
+                          <span className="rank">{val.rank}</span>
+                        </td>
+                        <td className="td-market-cap">
+                          <div className="market-cap-container">
+                            <div className="logo">
+                              <img src={val.icon} alt="logo" width={"24px"} />
+                            </div>
+                            <div className="symbol-and-cap-container">
+                              <p className="symbol">{val.symbol}</p>
+                              <span className="cap">{convertCurrency(val.marketCap)}</span>
+                            </div>
                           </div>
-                          <div className="symbol-and-cap-container">
-                            <p className="symbol">{val.symbol}</p>
-                            <span className="cap">{convertCurrency(val.marketCap)}</span>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="td-default">
-                        <p className="price">{"$" + (
-                          val.price > 1 ? val.price.toFixed(2) :
-                            val.price > 0.1 ? val.price.toFixed(4) :
-                              val.price > 0.01 ? val.price.toFixed(5) : val.price.toFixed(8))}</p>
-                      </td>
-                      <td className="td-default">
-                        <p className={val.priceChange1d < 0 ? "red" : "green"}>{val.priceChange1d}%</p>
-                      </td>
-                    </tr>
-                  </>
-                );
-              })}
-          </tbody>
-        </table>
+                        </td>
+                        <td className="td-default">
+                          <p className="price">{"$" + (
+                            val.price > 1 ? val.price.toFixed(2) :
+                              val.price > 0.1 ? val.price.toFixed(4) :
+                                val.price > 0.01 ? val.price.toFixed(5) : val.price.toFixed(8))}</p>
+                        </td>
+                        <td className="td-default">
+                          <p className={val.priceChange1d < 0 ? "red" : "green"}>{val.priceChange1d}%</p>
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        <div className="bottom" onClick={() => {changeTheme()}}></div>
+        <div className="navigationBar"></div>
       </div>
-      <div className="bottom"></div>
-      <div className="navigationBar"></div>
     </div>
   );
 }
